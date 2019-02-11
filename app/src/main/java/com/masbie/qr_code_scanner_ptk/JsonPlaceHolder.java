a44c810991ab4429653cd0c7bb0330ffecb22949
Anang Hanafi <ananghanafi13@gmail.com>
2019-02-11 23:48:30 +0700

    update

--- app/src/main/java/com/masbie/qr_code_scanner_ptk/JsonPlaceHolder.java
+++ app/src/main/java/com/masbie/qr_code_scanner_ptk/JsonPlaceHolder.java
@@ -10,7 +10,7 @@ import retrofit2.http.Query;
 public interface JsonPlaceHolder {
 
 //    @GET("detail?ptk_id=")
-@GET("detail")
+@GET("detail?")
 //@GET("detail?ptk_id=20253484185001")
     Call<GetPost> getPosts(@Query("ptk_id") String ptk_id);
 //Call<GetPost> getPosts();
